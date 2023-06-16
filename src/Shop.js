08cd47ff319475debff5f4d5508191f5e1f6bda0
korawit orkphol <korawit.orkphol@gmail.com>
2023-06-16 12:00:39 +0700

    Add shop with cart system

---
+++ src/Shop.js
@@ -0,0 +1,30 @@
+import './Shop.css';
+import { useState } from 'react';
+function Item(props){
+    return(<div onClick={()=>props.callback(props.id)}>
+        <img src={props.img} width={200} height={200}/><br/>
+        id: {props.id}<br/>
+        Name: {props.name}<br/>
+        Price: {props.price}<br/>
+    </div>);
+}
+export default function Shop(){
+    const [cart,setCart]=useState([]);
+    const handleClick=id=>{
+        alert("Add success!");
+        setCart([...cart,products[id]]);
+    }
+    const products=[
+        {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
+        {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
+        {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
+        {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
+        {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
+        {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
+        const productsList=products.map(item=><Item callback={handleClick} {...item}/>);
+        const cartList=cart.map(item=><li>{item.id} {item.name} {item.price}</li>)
+        return(<><div className="grid-container">{productsList}</div>
+              <h1>Cart</h1>
+              <ol>{cartList}</ol>
+        </>);
+}
